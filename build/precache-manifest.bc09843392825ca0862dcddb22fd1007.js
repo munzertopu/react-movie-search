991f7b36a0d1ae7a025412ee49def8438eeb680c
munzertopu <mmorshed@paradynamix.com>
2020-03-23 03:06:15 +0600

    ready for deploy

---
+++ build/precache-manifest.bc09843392825ca0862dcddb22fd1007.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e813522b1ad13a758da301f01965c1b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eefc93f453ca8a975039",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "d08fa92fe7f31333440d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "eefc93f453ca8a975039",
+    "url": "/static/js/2.5b3504c1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5b3504c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d08fa92fe7f31333440d",
+    "url": "/static/js/main.356af013.chunk.js"
+  },
+  {
+    "revision": "cb7eaf92ff2e02a72991",
+    "url": "/static/js/runtime-main.7ae4ffe7.js"
+  }
+]);
